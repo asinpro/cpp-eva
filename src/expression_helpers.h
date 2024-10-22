e9b3d2295431edd3e949e04cb8fdc6a7f70a8e67
asin.pro <axel.gto@outlook.com>
2024-10-22 17:54:15 +0200

    Documentation

--- src/expression_helpers.h
+++ src/expression_helpers.h
@@ -13,26 +13,10 @@
 #define none NONE
 
 #define ID_NAME(name) #name
-#define var(name, value) $var(#name, value)
-#define id(name) $id(ID_NAME(name))
 #define set(name, value) $set(#name, value)
 #define def(name, args, body) $def(#name, args, body)
-#define call(name, ...) $call(#name, __VA_ARGS__)
 #define inc(name) $inc(id(name))
 #define dec(name) $dec(id(name))
-#define cls(name, parent, body) $cls(#name, parent, body)
-#define prop(instance, member) $prop(#instance, #member)
-
-#define args() {}
-#define args1(a) $args(#a)
-#define args2(a, b) $args(#a, #b)
-#define args3(a, b, c) $args(#a, #b, #c)
-#define args4(a, b, c, d) $args(#a, #b, #c, #d)
-#define args5(a, b, c, d, e) $args(#a, #b, #c, #d, #e)
-#define args6(a, b, c, d, e, f) $args(#a, #b, #c, #d, #e, #f)
-#define args7(a, b, c, d, e, f, g) $args(#a, #b, #c, #d, #e, #f, #g)
-#define args8(a, b, c, d, e, f, g, h) $args(#a, #b, #c, #d, #e, #f, #g, #h)
-#define args9(a, b, c, d, e, f, g, h, i) $args(#a, #b, #c, #d, #e, #f, #g, #h, #i)
 
 template<typename ...Args>
 inline auto print(Args&&... args) {
@@ -72,9 +56,13 @@ inline auto floop(ExpressionPtr init, ExpressionPtr condition, ExpressionPtr mod
     return ForLoop::create(std::move(init), std::move(condition), std::move(modifier), std::move(body));
 }
 
+#define id(name) $id(ID_NAME(name))
+
 inline auto $id(std::string name) {
     return Identifier::create(std::move(name));
 }
+
+#define var(name, value) $var(#name, value)
 
 inline auto $var(std::string name, ExpressionPtr value) {
     return VariableDeclaration::create(std::move(name), std::move(value));
@@ -232,6 +220,17 @@ inline auto $args(Args&&... args) {
     return names;
 }
 
+#define args() {}
+#define args1(a) $args(#a)
+#define args2(a, b) $args(#a, #b)
+#define args3(a, b, c) $args(#a, #b, #c)
+#define args4(a, b, c, d) $args(#a, #b, #c, #d)
+#define args5(a, b, c, d, e) $args(#a, #b, #c, #d, #e)
+#define args6(a, b, c, d, e, f) $args(#a, #b, #c, #d, #e, #f)
+#define args7(a, b, c, d, e, f, g) $args(#a, #b, #c, #d, #e, #f, #g)
+#define args8(a, b, c, d, e, f, g, h) $args(#a, #b, #c, #d, #e, #f, #g, #h)
+#define args9(a, b, c, d, e, f, g, h, i) $args(#a, #b, #c, #d, #e, #f, #g, #h, #i)
+
 template<typename ...Args>
 inline auto vars(Args&&... args) {
     std::vector<ExpressionPtr> expressions;
@@ -239,34 +238,151 @@ inline auto vars(Args&&... args) {
     return expressions;
 }
 
+/**
+ * @brief Call function
+ *
+ * @param name std::string
+ * @param args std::vector<ExpressionPtr>
+ * @return FunctionCallPtr
+ *
+ * @code
+ * $call("functionName", vars(arg1, arg2, arg3));
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * call(functionName, vars(arg1, arg2, arg3));
+ * @endcode
+ */
 inline auto $call(std::string name, std::vector<ExpressionPtr> args) {
     return FunctionCall::create(std::move(name), std::move(args));
 }
 
+/**
+ * @brief Call function
+ *
+ * @param name std::string
+ * @param args Args&&...
+ * @return FunctionCallPtr
+ *
+ * @code
+ * $call("functionName", arg1, arg2, arg3);
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * call(functionName, arg1, arg2, arg3);
+ * @endcode
+ */
 template<typename ...Args>
 inline auto $call(std::string name, Args&&... args) {
     return $call(std::move(name), vars(std::forward<Args>(args)...));
 }
 
+#define call(name, ...) $call(#name, __VA_ARGS__)
+
+/**
+ * @brief Call lambda inplace - Immediately Invoked Lambda Expression (IILE)
+ *
+ * @param exp FunctionDeclarationPtr
+ * @param args std::vector<ExpressionPtr>
+ * @return AnonymousFunctionCallPtr
+ *
+ * @code
+ * iile(lambda($args("a", "b"), add(id(a), id(b))), vars(1, 2));
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * iile(lambda(args2(a, b), add(id(a), id(b))), vars(1, 2));
+ * @endcode
+ */
 inline auto iile(FunctionDeclarationPtr exp, std::vector<ExpressionPtr> args) {
     return AnonymousFunctionCall::create(std::move(exp), std::move(args));
 }
 
+/**
+ * @brief Call lambda inplace - Immediately Invoked Lambda Expression (IILE)
+ *
+ * @param exp FunctionDeclarationPtr
+ * @param args Args&&...
+ * @return AnonymousFunctionCallPtr
+ *
+ * @code
+ * iile(lambda($args(a, b), add(id(a), id(b))), 1, 2);
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * iile(lambda(args2(a, b), add(id(a), id(b)), 1, 2);
+ * @endcode
+ */
 template<typename ...Args>
 inline auto iile(FunctionDeclarationPtr exp, Args&&... args) {
     return iile(std::move(exp), vars(std::forward<Args>(args)...));
 }
 
+/**
+ * @brief Create new switch statement
+ *
+ * @param condition ExpressionPtr
+ * @param body ExpressionPtr
+ * @return std::pair<ExpressionPtr, ExpressionPtr>
+ *
+ * @code
+ * when(eq($id("a"), $id("b")), 1);
+ * when(eq($id("a"), 1)), 1);
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * when(eq(id(a), id(b)), 1);
+ * when(eq(id(a), 1)), 1);
+ * @endcode
+ */
 template<typename T>
 inline std::pair<ExpressionPtr, ExpressionPtr> when(ExpressionPtr condition, T&& body) {
     return std::make_pair(std::move(condition), wrap(std::forward<T>(body)));
 }
 
+/**
+ * @brief Create new switch statement with default case
+ *
+ * @param value ExpressionPtr
+ * @return std::pair<ExpressionPtr, ExpressionPtr>
+ *
+ * @code
+ * any(3);
+ * @endcode
+ */
 template<typename T>
 inline std::pair<ExpressionPtr, ExpressionPtr> any(T&& value) {
     return std::make_pair(TRUE, wrap(std::forward<T>(value)));
 }
 
+/**
+ * @brief Create new switch statement
+ *
+ * @param args std::pair<ExpressionPtr, ExpressionPtr>
+ * @return SwitchPtr
+ *
+ * @code
+ * select(
+ *    when(eq($id("a"), $id("b")), 1),
+ *    when(eq($id("a"), $id("c")), 2),
+ *    any(3)
+ * );
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * select(
+ *    when(eq(id(a), id(b)), 1),
+ *    when(eq(id(a), id(c)), 2),
+ *    any(3)
+ * );
+ * @endcode
+ */
 template<typename ...Args>
 inline auto select(Args&&... args) {
     auto cases = std::vector<std::pair<ExpressionPtr, ExpressionPtr>>();
@@ -274,17 +390,51 @@ inline auto select(Args&&... args) {
     return Switch::create(std::move(cases));
 }
 
+#define cls(name, parent, body) $cls(#name, parent, body)
 
+/**
+ * @brief Create new class declaration
+ *
+ * @param name std::string
+ * @param parent IdentifierPtr
+ * @param body BlockPtr
+ *
+ * @code
+ * $cls("ClassName", parent, body);
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * cls(ClassName, parent, body);
+ * @endcode
+ */
 inline auto $cls(std::string name, IdentifierPtr parent, BlockPtr body) {
     return ClassDeclaration::create(std::move(name), std::move(parent), std::move(body));
 }
 
-// Create new instance of class
-// Usage: newi("ClassName", {arg1, arg2, arg3})
-// Example: newi("Point", {lit(1), lit(2)})
-inline auto newi(std::string name, std::vector<ExpressionPtr> args) {
+#define newi(name, args) $newi(#name, args)
+
+/**
+ * @brief Create new instance of class
+ *
+ * @param name std::string
+ * @param args std::vector<ExpressionPtr>
+ * @return NewInstancePtr
+ *
+ * @code
+ * $newi("ClassName", vars(arg1, arg2, arg3));
+ * @endcode
+ *
+ * @code
+ * // Macro usage example
+ * newi(ClassName, vars(arg1, arg2, arg3));
+ * @endcode
+ */
+inline auto $newi(std::string name, std::vector<ExpressionPtr> args) {
     return NewInstance::create(std::move(name), std::move(args));
 }
+
+#define prop(instance, member) $prop(#instance, #member)
 
 /**
  * @brief Access member in class instance
@@ -294,12 +444,12 @@ inline auto newi(std::string name, std::vector<ExpressionPtr> args) {
  * @return MemberAccessPtr
  *
  * @code
- * auto member = $prop("instance", "member");
+ * $prop("instance", "member");
  * @endcode
  *
  * @code
  * // Macro usage example
- * auto member = prop(instance, member);
+ * prop(instance, member);
  * @endcode
  */
 inline auto $prop(std::string instance, std::string member) {
@@ -314,19 +464,18 @@ inline auto $prop(std::string instance, std::string member) {
  * @return AssignmentPtr
  *
  * @code
- * auto assignment = $set($prop("instance", "member"), value);
+ * $set($prop("instance", "member"), value);
  * @endcode
  *
  * @code
  * // Macro usage example
- * auto assignment = setm(prop(instance, member), value);
+ * setm(prop(instance, member), value);
  * @endcode
  */
 inline auto setm(MemberAccessPtr member, ExpressionPtr value) {
     return $set(std::move(member), std::move(value));
 }
 
-// Call member function in class instance
 /**
  * @brief Call member function in class instance
  *
@@ -335,12 +484,12 @@ inline auto setm(MemberAccessPtr member, ExpressionPtr value) {
  * @return MemberFunctionCallPtr
  *
  * @code
- * auto call = callm($prop("instance", "method"), vars(arg1, arg2, arg3));
+ * callm($prop("instance", "method"), vars(arg1, arg2, arg3));
  * @endcode
  *
  * @code
  * // Macro usage example
- * auto call = callm(prop(instance, method), vars(arg1, arg2, arg3));
+ * callm(prop(instance, method), vars(arg1, arg2, arg3));
  * @endcode
  */
 inline auto callm(MemberAccessPtr member, std::vector<ExpressionPtr> args) {
--- src/tests/class_test.h
+++ src/tests/class_test.h
@@ -9,15 +9,16 @@ void runClassTest(Eva& eva) {
     IASSERT(
             beg(
                     cls(Point, NONE, beg(
-                                def(constructor, args3(self, x, y), beg(
-                                            setm(prop(self, x), id(x)),
-                                            setm(prop(self, y), id(y))
-                                    )),
-                                def(calc, args1(self), beg(
-                                        add(prop(self, x), prop(self, y))
-                                    ))
-                        )),
-                    var(p, newi("Point", vars(10, 20))),
+                            def(constructor, args3(self, x, y), beg(
+                                    setm(prop(self, x), id(x)),
+                                    setm(prop(self, y), id(y))
+                            )),
+
+                            def(calc, args1(self), beg(
+                                    add(prop(self, x), prop(self, y))
+                            ))
+                    )),
+                    var(p, newi(Point, vars(10, 20))),
                     callm(prop(p, calc), vars(id(p)))
             )
     , 30);
